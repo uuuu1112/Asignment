6349ed743be4f9d7e2dfd07d550e315d03afb871
uuuu1112 <54828912+uuuu1112@users.noreply.github.com>
2021-03-08 15:20:25 +0800

    Create opensource.py

---
+++ week3/opensource.py
@@ -0,0 +1,13 @@
+import urllib.request as request
+import json 
+
+src="https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json"
+
+with request.urlopen(src) as response:
+    data=json.load(response)
+    
+clist=data["result"]["results"]
+with open("data.txt","w",encoding="utf-8") as file:
+    for viewpoint in clist:
+        #print(viewpoint["stitle"])
+        file.write(viewpoint["stitle"]+","+viewpoint["longitude"]+","+viewpoint["latitude"]+","+viewpoint["file"].replace("jpg","jpg#").split("#")[0]+"\n")
